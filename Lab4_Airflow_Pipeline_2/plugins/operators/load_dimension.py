4f374d1a15b33ce842f4d9de9a8faa44dc318e6d
husthunterpy01 <dangminhhust193231@gmail.com>
2024-10-14 23:30:21 +0700

    Update the 1st script for airflow data pipeline

---
+++ Lab4_Airflow_Pipeline_2/plugins/operators/load_dimension.py
@@ -0,0 +1,30 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id = " ",
+                 table = " ",
+                 sql_query = " ",
+                 mode = "insert-data",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql_query = sql_query
+        self.mode = mode
+
+    def execute(self, context):
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        if (self.mode == "truncate-data"):
+            self.log.info(f"Truncating the table {self.table}")
+            redshift_hook.run("TRUNCATE TABLE {}").format{self.table}
+        
+        self.log.info(f"Inserting the dimension table {self.table}")
+        redshift_hook.run("INSERT TABLE {} {}".format(self.table, self.sql_query))
